5a27ef8c6e6db26138e43dee85678fe45a828a78
Fedoseev Danil <hanriel59@gmail.com>
2026-07-21 23:19:42 +0500

    hotfix(Manifest): fix access

--- proxy.ts
+++ proxy.ts
@@ -1,16 +1,16 @@
 import { auth } from "@/auth";
 
 export const proxy = auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/login" && req.nextUrl.pathname !== "/register") {
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
     const newUrl = new URL("/login", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
-  if (req.auth && (req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/register")) {
+  if (req.auth && req.nextUrl.pathname === "/login") {
     const newUrl = new URL("/", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 });
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|icons|manifest.json).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|icons|manifest.webmanifest).*)"],
 };
